0056d297298b64d21717b6f37d7a184d5bba119d
Wyatt VO <votranquy96@gmail.com>
2023-11-20 15:28:37 +0700

    Render data to App

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="wrapper">
+    <div 
+      v-for="airport in airports" 
+      :key="airport.abbreviation" 
+      class="card"
+    >
+      <p>{{ airport.abbreviation }}</p>
+      <p>{{ airport.name }}</p>
+      <p>{{ airport.city }}, {{ airport.state }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+import data from '@/data/us-airports.js'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  setup() {
+    const airports = ref(data)
+    return { airports }
   }
 }
 </script>
@@ -23,4 +34,30 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+.wrapper {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-column-gap: 1rem;
+  max-width: 960px;
+  margin: 0 auto;
+}
+
+.card {
+  border: 3px solid;
+  border-radius: .5rem;
+  padding: 1rem;
+  margin-bottom: 1rem;
+}
+
+.card p:first-child {
+  font-weight: bold;
+  font-size: 2.5rem;
+  margin: 1rem 0;
+}
+
+.card p:last-child {
+  font-style: italic;
+  font-size: .8rem;
+}
+
 </style>
