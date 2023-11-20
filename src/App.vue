4ef0c04b17124715ed7e2b79d938e3cfdf16f7b1
Wyatt VO <votranquy96@gmail.com>
2023-11-20 15:37:25 +0700

    Create single-file component AirportCards

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,15 @@
 <template>
-  <div class="wrapper">
-    <div 
-      v-for="airport in airports" 
-      :key="airport.abbreviation" 
-      class="card"
-    >
-      <p>{{ airport.abbreviation }}</p>
-      <p>{{ airport.name }}</p>
-      <p>{{ airport.city }}, {{ airport.state }}</p>
-    </div>
-  </div>
+  <AirportCards />
+  <airport-cards />
 </template>
 
 <script>
-import { ref } from 'vue'
-import data from '@/data/us-airports.js'
+import AirportCards from '@/components/AirportCards.vue'
 
 export default {
   name: 'App',
-  setup() {
-    const airports = ref(data)
-    return { airports }
+  components: {
+    AirportCards
   }
 }
 </script>
@@ -34,30 +23,4 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-.wrapper {
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  grid-column-gap: 1rem;
-  max-width: 960px;
-  margin: 0 auto;
-}
-
-.card {
-  border: 3px solid;
-  border-radius: .5rem;
-  padding: 1rem;
-  margin-bottom: 1rem;
-}
-
-.card p:first-child {
-  font-weight: bold;
-  font-size: 2.5rem;
-  margin: 1rem 0;
-}
-
-.card p:last-child {
-  font-style: italic;
-  font-size: .8rem;
-}
-
 </style>
---
+++ src/components/AirportCards.vue
@@ -0,0 +1,52 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div class="wrapper">
+    <div v-for="airport in airports" :key="airport.abbreviation" class="card">
+      <p>{{ airport.abbreviation }}</p>
+      <p>{{ airport.name }}</p>
+      <p>{{ airport.city }}, {{ airport.state }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+import data from '@/data/us-airports.js'
+
+export default {
+  name: 'AirportCards',
+  setup() {
+    const airports = ref(data)
+
+    return { airports }
+  }
+}
+</script>
+
+<style scoped>
+.wrapper {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-column-gap: 1rem;
+  max-width: 960px;
+  margin: 0 auto;
+}
+
+.card {
+  border: 3px solid;
+  border-radius: .5rem;
+  padding: 1rem;
+  margin-bottom: 1rem;
+}
+
+.card p:first-child {
+  font-weight: bold;
+  font-size: 2.5rem;
+  margin: 1rem 0;
+}
+
+.card p:last-child {
+  font-style: italic;
+  font-size: .8rem;
+}
+</style>
